ea5b6164faccf032d598d7a0ac703603730745fd
raghu <gs.raghuram@yahoo.com>
2020-05-28 17:09:00 +1000

    update

---
+++ get.js
@@ -0,0 +1,20 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamo-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      userid: event.requestContext.identity.cognitoIdentityId,
+      designid: event.pathParameters.id
+    }
+  };
+
+  const result = await dynamoDb.get(params);
+  if ( ! result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  // Return the retrieved item
+  return result.Item;
+});
